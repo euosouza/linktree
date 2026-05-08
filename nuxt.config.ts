65d81ea57afd85ef29caea30292f47df8f697e51
Victor Souza <euosouza@gmail.com>
2026-05-08 18:34:49 -0300

    buil: ajuste icons build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,17 +16,37 @@ export default defineNuxtConfig({
     }
   },
   icon: {
+    // 1. Força o download de todas as coleções usadas durante o build
+    serverBundle: 'remote',
+
+    // 2. Garante que o bundle de cliente contenha os ícones para uso offline/estático
     clientBundle: {
-      scan: true,
+      // Liste explicitamente os ícones que falharam
       includeCustomCollections: true,
+      icons: [
+        'ph:instagram-logo',
+        'ph:envelope-simple',
+        'ph:house-fill',
+        'ph:first-aid-fill',
+        'ph:star-fill',
+        'ph:question-fill',
+        'ph:caret-down-bold',
+        'ph:first-aid',
+        'ph:syringe',
+        'ph:stethoscope',
+        'ph:check-circle',
+        'ph:star',
+        'ph:smiley-wink'
+      ],
+      scan: true, // Escaneia automaticamente seus arquivos .vue
     },
-    serverBundle: {
-      collections: ['ph']
-    }
   },
   router: {
     options: {
       scrollBehaviorType: 'smooth'
     }
   },
+  routeRules: {
+    '/': { prerender: true }
+  }
 })
